76e89dc19df7c5f8880fdd3a6181a5f2458c6117
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2022-04-27 14:03:22 -0400

    Set the main script run settings for prelimnary runs

--- lib_secure_monitoring_service/__main__.py
+++ lib_secure_monitoring_service/__main__.py
@@ -1,13 +1,13 @@
 from lib_bgp_simulator import Simulator, BGPAS, Graph
 from lib_secure_monitoring_service.engine_inputs import V4SubprefixHijack
-from lib_secure_monitoring_service.rov_sms import ROVSMS
+from lib_secure_monitoring_service.rov_sms import ROVSMS, ROVSMSK1
 from lib_rovpp import ROVPPV1SimpleAS
 
 def main():
-    Simulator().run(graphs=[Graph(percent_adoptions=[80],
-                                  adopt_as_classes=[ROVSMS],
+    Simulator().run(graphs=[Graph(percent_adoptions=[0,5,10,20,40,60,80,100],
+                                  adopt_as_classes=[ROVPPV1SimpleAS, ROVSMS, ROVSMSK1],
                                   EngineInputCls=V4SubprefixHijack,
-                                  num_trials=10,
+                                  num_trials=20,
                                   BaseASCls=BGPAS)]
                     )
 
