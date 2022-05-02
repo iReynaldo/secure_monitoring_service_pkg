fd512e1296cee92802e52174bd820533435404e7
Reynaldo Morillo <reymorillo2@gmail.com>
2022-05-02 16:45:19 -0400

    Created a script to collect all the system tests images into a single directory structure (only with the PNGs). This will be used for overleaf when documenting the system tests.

---
+++ lib_secure_monitoring_service/tests/system_tests/collect_all_test_results.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+output_filename="all_tests.md"
+output_dir="system_test_images"
+
+for hijack_type in v4_subprefix_hijack ;
+do
+    for sub_type in $(find $hijack_type -mindepth 1 -maxdepth 1 -type d) ;
+    do  
+        for sub_sub_type in $(find $sub_type -mindepth 1 -maxdepth 1 -type d) ;
+        do
+            for graph in $(find $sub_sub_type -mindepth 1 -maxdepth 1 -type d) ;
+            do
+                for policy_dir in $(find $graph -mindepth 1 -maxdepth 1 -type d | grep -v pycache ) ;
+                do
+                    for propapation_round_dir in $(find $policy_dir -mindepth 1 -maxdepth 1 -type d | grep -v pycache ) ;
+                    do
+                        for png in $(find $propapation_round_dir -mindepth 1 -maxdepth 1  | grep ".png$") ;
+                        do
+                            curr_output_dir=$output_dir/$propapation_round_dir;
+                            echo $curr_output_dir
+                            mkdir -p $curr_output_dir
+                            # Extract the PNG
+                            cp $png $curr_output_dir
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
+
