c27f45a0deef7418f9c66c2b9010e731c05c6b1d
Reynaldo Morillo <reymorillo2@gmail.com>
2022-05-10 15:12:04 -0400

    Created system tests to expose V4 Withdrawl problem
    The tests were designed to highlight a problem with current implementation of v4 (i.e. withdrawls needed for the /16). However, they actually expose another issue, which is the necessity of waiting for all the reports to stabilize prior to creating the avoid list and applying recommendations to ASes.

---
+++ lib_secure_monitoring_service/tests/system_tests/v4_subprefix_hijack/hidden_hijack_tests/by_rov/graph_041/test_hidden_hijack.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import pytest
+
+from lib_bgp_simulator import BaseGraphSystemTester
+from lib_bgp_simulator import BGPSimpleAS
+from lib_bgp_simulator import Graph041
+
+from lib_rovpp import ROVPPV1SimpleAS
+
+from lib_secure_monitoring_service.tests.system_tests.unstable import Unstable
+from lib_secure_monitoring_service.rov_sms import ROVSMS, ROVSMSK1
+from lib_secure_monitoring_service.engine_inputs import V4SubprefixHijack
+
+
+class BaseHiddenHijackTester(Unstable, BaseGraphSystemTester):
+    GraphInfoCls = Graph041
+    BaseASCls = BGPSimpleAS
+    EngineInputCls = V4SubprefixHijack
+    base_dir = Path(__file__).parent
+    adopting_asns = (32, 33, 55, )
+    rov_adopting_asns = (77, )
+
+
+class Test003HiddenHijackROVPPV1(BaseHiddenHijackTester):
+    AdoptASCls = ROVPPV1SimpleAS
+
+
+class Test004HiddenHijackROVSMS(BaseHiddenHijackTester):
+    AdoptASCls = ROVSMS
+
