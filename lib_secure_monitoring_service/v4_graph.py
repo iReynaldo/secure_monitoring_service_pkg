2ac713097b761a799887190ddf56c85b2e8a8c2f
Reynaldo Morillo <reymorillo2@gmail.com>
2022-05-15 23:34:22 -0400

    error in print statement

--- lib_secure_monitoring_service/v4_graph.py
+++ lib_secure_monitoring_service/v4_graph.py
@@ -22,8 +22,7 @@ class V4Graph(Graph):
                                                   engine,
                                                   percent_adopt)
             for ASCls in self.adopt_as_classes:
-                print(f
-                "{percent_adopt}% {ASCls.name}, #{trial}",
+                print(f"{percent_adopt}% {ASCls.name}, #{trial}",
                 end = "                             " + "\r")
                 # Deepcopy input to make sure input is fresh
                 engine_input = deepcopy(og_engine_input)
