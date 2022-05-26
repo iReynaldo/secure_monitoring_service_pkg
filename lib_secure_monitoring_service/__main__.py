ee03da3ceb0ec587ee5e8b633582bdcd9ccf0913
Reynaldo Morillo <reymorillo2@gmail.com>
2022-05-26 10:47:17 -0400

    Changed main to use multiple processes.

--- lib_secure_monitoring_service/__main__.py
+++ lib_secure_monitoring_service/__main__.py
@@ -1,4 +1,4 @@
-from lib_bgp_simulator import Simulator, BGPAS, Graph
+from lib_bgp_simulator import Simulator, BGPAS, Graph, MPMethod
 
 from lib_rovpp import ROVPPV1SimpleAS
 
@@ -9,10 +9,11 @@ from lib_secure_monitoring_service.v4_graph import V4Graph
 
 def main():
     Simulator().run(graphs=[V4Graph(percent_adoptions=[0,5,10,20,40,60,80,100],
-                                    adopt_as_classes=[ROVPPV1SimpleAS, ROVSMS, ROVSMSK1, ROVSMSK2, ROVSMSK3],
+                                    adopt_as_classes=[ROVPPV1SimpleAS, ROVSMS],
                                     EngineInputCls=V4SubprefixHijack,
                                     num_trials=20,
-                                    BaseASCls=BGPAS)]
+                                    BaseASCls=BGPAS)],
+                    mp_method = MPMethod.SINGLE_PROCESS
                     )
 
 
