f347cb7cb87bb576cda78fe3f8f651af90a6ba1a
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2022-06-27 11:40:02 -0400

    Created a script to make deterministics runs.

--- lib_secure_monitoring_service/__main__.py
+++ lib_secure_monitoring_service/__main__.py
@@ -1,19 +1,23 @@
+import random
+
 from lib_bgp_simulator import Simulator, BGPAS, Graph, MPMethod
 
-from lib_rovpp import ROVPPV1SimpleAS
+from lib_rovpp import ROVPPV1SimpleAS, ROVPPV1LiteSimpleAS
 
 from lib_secure_monitoring_service.engine_inputs import V4SubprefixHijack
-from lib_secure_monitoring_service.rov_sms import ROVSMS, ROVSMSK1, ROVSMSK2, ROVSMSK3, ROVSMSK4, ROVSMSK5, ROVSMSK6, ROVSMSK7, ROVSMSK10, ROVSMSK20
-from lib_secure_monitoring_service.rov_sms import ROVSMSK20000, ROVSMSK30000
-
-from lib_secure_monitoring_service.rov_sms import ROVSMSK30, ROVSMSK50, ROVSMSK70, ROVSMSK100, ROVSMSK150, ROVSMSK200, ROVSMSK300, ROVSMSK500, ROVSMSK1000, ROVSMSK2000, ROVSMSK5000
-
+from lib_secure_monitoring_service.rov_sms import ROVSMS, ROVSMSK1, ROVSMSK2, ROVSMSK3, ROVSMSK4, ROVSMSK5, ROVSMSK6, ROVSMSK7, ROVSMSK10,
+from lib_secure_monitoring_service.rov_sms import ROVSMSK20, ROVSMSK30, ROVSMSK50, ROVSMSK70, ROVSMSK100
+from lib_secure_monitoring_service.rov_sms import ROVSMSK150, ROVSMSK200, ROVSMSK300, ROVSMSK500, ROVSMSK1000, ROVSMSK2000, ROVSMSK5000, ROVSMSK20000, ROVSMSK30000
 from lib_secure_monitoring_service.v4_graph import V4Graph
 
 
+# Set Random Seed to determinitic runs
+# Note: The PYTHONHASHSEED also needs to set in the shell environment!!!!!!!!!
+random.seed(0)
+
 def main():
     Simulator().run(graphs=[V4Graph(percent_adoptions=[0,5,10,20,40,60,80,100],
-                                    adopt_as_classes=[ROVPPV1SimpleAS, ROVSMSK500, ROVSMSK1000, ROVSMSK2000, ROVSMSK5000, ROVSMSK20000, ROVSMSK30000],
+                                    adopt_as_classes=[ROVPPV1LiteSimpleAS, ROVSMS, ROVSMSK1, ROVSMSK10],
                                     EngineInputCls=V4SubprefixHijack,
                                     num_trials=10,
                                     BaseASCls=BGPAS)],
---
+++ lib_secure_monitoring_service/deterministic_runs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export PYTHONHASHSEED=0
+pypy __main__.py
