5b3bbfc1ae4864d5a488ccde4b6920de6ba045b1
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2022-09-27 19:35:15 -0400

    Added commas to the end of each JSON and put quotation around the policy

--- secure_monitoring_service_pkg/tests/performance_tests/run_performance_tests.sh
+++ secure_monitoring_service_pkg/tests/performance_tests/run_performance_tests.sh
@@ -13,7 +13,7 @@ do
         do
             echo "Running ${policy} at ${percent}% adoption for ${num_trials} trials"
             # time pypy -m cProfile -o "performance_${policy}_${percent}_percent_${num_trials}_trial.profile" performance_tester.py $policy $percent $num_trials
-            formatted_string=$(echo '{\n"policy":' ${policy} ',\n"percentage":' ${percent} ',\n"trials":' $num_trials ',\n"avg_unshared_memory": %D,\n"avg_memory": %K,\n"avg_set_memory": %t,\n"max_memory": %M,\n"total_time": %e,\n"cpu": "%P"\n}')
+            formatted_string=$(echo '{\n"policy":"' ${policy} '",\n"percentage":' ${percent} ',\n"trials":' $num_trials ',\n"avg_unshared_memory": %D,\n"avg_memory": %K,\n"avg_set_memory": %t,\n"max_memory": %M,\n"total_time": %e,\n"cpu": "%P"\n},')
             time -f "${formatted_string}" pypy performance_tester.py ${policy} ${percent} ${num_trials}
         done
     done
