18bed7d072d32a6879551539c2c35fc629dd2a76
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2022-12-04 14:32:02 -0500

    Updated run_benchmarks script to require the tag and print usage information. It now also requires the scenario to be specified.

--- secure_monitoring_service_pkg/tests/performance_tests/benchmark/run_benchmarks.sh
+++ secure_monitoring_service_pkg/tests/performance_tests/benchmark/run_benchmarks.sh
@@ -1,9 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 # IMPORTANT NOTE:
 # Make sure the python environment is activated before this script is run
 
-tag=${1}
+
+####################################
+# Environment Variables
+####################################
+
+PYTHONHASHSEED=0;
+
+
+####################################
+# Input Arguments
+####################################
+
+# Check number of args is satisfied
+if [[ "$#" != "2" || ( $@ == "--help" ) ||  $@ == "-h"  ]]; then
+    echo "USAGE: ${0} [scenario] [tag] | [-h | --help]"
+    echo "scenario: V4SubprefixHijackScenario | SubprefixAutoImmuneScenario"
+    exit 1
+fi
+
+# Set Input Argument Variables
+scenario=${1}
+tag=${2}  # Optional Argument
+
+
+####################################
+# Main Loop
+####################################
+
 
 for policy in v1 v4 v4k1 v4k5
 do
@@ -12,7 +39,7 @@ do
         for num_trials in 10 20 30 60 100
         do
             echo "Running ${policy} at ${percent}% adoption for ${num_trials} trials"
-            pypy probe.py --policy ${policy} --percentages ${percent} --num_trials ${num_trials} --cpus 12 --tag ${tag}
+            pypy probe.py --scenario ${scenario} --policy ${policy} --percentages ${percent} --num_trials ${num_trials} --cpus 12 --tag ${tag}
         done
     done
 done
