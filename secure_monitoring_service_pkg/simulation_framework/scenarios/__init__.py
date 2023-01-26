02e3f49291669b86bc479fc9f5305edf00023597
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2023-01-26 15:38:44 -0500

    Fixed import error

--- secure_monitoring_service_pkg/simulation_framework/scenarios/__init__.py
+++ secure_monitoring_service_pkg/simulation_framework/scenarios/__init__.py
@@ -2,7 +2,7 @@ from .hijack_scenarios import V4SubprefixHijackScenario
 from .hijack_scenarios import SubprefixAutoImmuneScenario
 from .v4_scenario import V4Scenario
 from .cdn import CDN
-from peer import Peer
+from .peer import Peer
 
 __all__ = [
     "V4SubprefixHijackScenario",
