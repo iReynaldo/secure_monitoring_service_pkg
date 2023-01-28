7480e64cb5e7deec6f9c3bb108cfba836c7e01cf
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2023-01-28 12:20:39 -0500

    Potentially solved invalid vertex bug
    The error occurred because the reported path had a loop in it.
    
    This occurs because the receive_ann function in ROVSMS reports a subprefix prepending the receiving ASes asn without considering that the AS would just drop this announcement it's on the path.
    
    Now ROVSMS will not report if its already on the path.

--- secure_monitoring_service_pkg/simulation_engine/as_classes/rov_sms.py
+++ secure_monitoring_service_pkg/simulation_engine/as_classes/rov_sms.py
@@ -27,8 +27,9 @@ class ROVSMS(ROVPPV1LiteSimpleAS):
         if ann.invalid_by_roa:
             logger.debug(f"ASN {self.asn} sending report about {ann.prefix}")
             if self._max_num_dishonest_nodes > 0:
-                adjusted_as_path = (self.asn,) + ann.as_path
-                report = Report(reporting_asn=self.asn, prefix=ann.prefix, as_path=adjusted_as_path)
+                if self.asn not in ann.as_path:
+                    adjusted_as_path = (self.asn,) + ann.as_path
+                    report = Report(reporting_asn=self.asn, prefix=ann.prefix, as_path=adjusted_as_path)
             else:
                 report = Report(reporting_asn=self.asn, prefix=ann.prefix, as_path=ann.as_path)
             self.trusted_server.receive_report(report)
