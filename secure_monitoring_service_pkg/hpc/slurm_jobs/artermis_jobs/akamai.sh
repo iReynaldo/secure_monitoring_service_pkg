fec52ea58499302b14ca0e5002753cca8bbd7fda
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2023-04-22 23:49:24 -0400

    Added slurm jobs for Artemis

---
+++ secure_monitoring_service_pkg/hpc/slurm_jobs/artermis_jobs/akamai.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition=hi-core                   # Name of partition
+#SBATCH --ntasks=50                            # Request Number of CPU cores
+#SBATCH --mail-type=BEGIN,END,FAIL              # Event(s) that triggers email notification (BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=reynaldo.morillo@uconn.edu  # Destination email address
+
+# Setup python environment
+source /home/rjm11010/miniconda3/etc/profile.d/conda.sh
+conda activate v4sims
+
+export PYTHONHASHSEED=0
+python ../../python_scripts/artemis_main.py akamai
