2a80a3ad44691bef9780668264b97bbb81926493
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2023-04-25 18:26:14 -0400

    Added hpc scripts for getting the basic subprefix results
    The setting right now does not assume the relays are reachable; which can be a runtime/memory problem for HPC ...

--- secure_monitoring_service_pkg/hpc/python_scripts/basic_subprefix_hijack.py
+++ secure_monitoring_service_pkg/hpc/python_scripts/basic_subprefix_hijack.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from pathlib import Path
-from tempfile import TemporaryDirectory
 import time
-import json
+import sys
 
 from bgp_simulator_pkg import ROVSimpleAS
 
@@ -28,13 +27,48 @@ BASE_PATH = Path("~/Desktop/graphs/").expanduser()
 # Adopting settings
 adoption_settings = {
     "adopters_for_1_attackers": [ROVSimpleAS, ROVPPV1LiteSimpleAS, ROVSMS],
-    "adopters_for_2_attackers": [ROVPPV1LiteSimpleAS, ROVSMSK1, ROVSMSK2, ROVSMSK3],
-    "adopters_for_5_attackers": [ROVPPV1LiteSimpleAS, ROVSMSK1, ROVSMSK5, ROVSMSK10]
+    "adopters_for_2_attackers": [ROVSimpleAS, ROVPPV1LiteSimpleAS, ROVSMSK1, ROVSMSK2, ROVSMSK3],
+    "adopters_for_5_attackers": [ROVSimpleAS, ROVPPV1LiteSimpleAS, ROVSMSK2, ROVSMSK5, ROVSMSK10]
 }
 
 # Scenario options
 AUTOIMMUNE = "SubprefixAutoImmuneScenario"
 SUBPREFIX_HIJACK = "V4SubprefixHijackScenario"
+
+
+############################
+# Arguments
+############################
+
+if len(sys.argv) != 4:
+    raise "This script needs 3 arguments " \
+          "[rov setting, overlay setting, attack/no-attacker relay]"
+
+rov_setting_raw = sys.argv[1].lower()  # none / real
+if rov_setting_raw == 'none':
+    rov_setting = False
+elif rov_setting_raw == 'real':
+    rov_setting = True
+else:
+    raise "Unknown ROV setting given"
+
+overlay_setting_raw = sys.argv[2].lower()  # None, akamai, cloudflare ..., five, ten, twenty, ...
+if overlay_setting_raw == 'none':
+    overlay_setting = None
+elif overlay_setting_raw in ['akamai', 'cloudflare', 'verisign', 'incapsula', 'neustar']:
+    overlay_setting = CDN.__getattribute__(overlay_setting_raw)
+elif overlay_setting_raw in ['five', 'ten', 'twenty', 'hundred']:
+    overlay_setting = Peer.__getattribute__(overlay_setting_raw)
+else:
+    raise "Unknown Overlay setting given"
+
+attack_relay_raw = sys.argv[3].lower()  # True / False
+if attack_relay_raw == 'true':
+    attack_relay = True
+elif attack_relay_raw == 'false':
+    attack_relay = False
+else:
+    raise "Unknown attack_relay setting given"
 
 
 #############################
@@ -47,6 +81,8 @@ def process_experiment_settings(simulation_kwargs, scenario_kwargs, other_settin
     del simulation_kwargs["subgraphs"]  # We don't need to output this
     simulation_kwargs["caida_kwargs"] = str(simulation_kwargs["caida_kwargs"])
     settings.update(simulation_kwargs)
+    scenario_kwargs["relay_asns"] = list(scenario_kwargs["relay_asns"]) if len(
+        scenario_kwargs["relay_asns"]) <= 5 else [len(scenario_kwargs["relay_asns"]), ]
     settings.update(scenario_kwargs)
     return settings
 
@@ -58,17 +94,17 @@ def process_experiment_settings(simulation_kwargs, scenario_kwargs, other_settin
 def other_settings():
     settings = {
         "scenario": SUBPREFIX_HIJACK,
-        "output_filename": "basic_subprefix_hijack_with_rov_v1lite_and_k0_50_trials"
+        "output_filename": "basic_subprefix_hijack_with_rov_v1lite_k2-5-10"
     }
     return settings
 
 
 def scenario_kwargs():
     settings = {
-        "num_attackers": 1,
-        "min_rov_confidence": 1000,
+        "num_attackers": 5,
+        "min_rov_confidence": 0 if rov_setting else 1000,
         "adoption_subcategory_attrs": ("stub_or_mh_ases", "etc_ases", "input_clique_ases"),
-        "relay_asns": None,
+        "relay_asns": overlay_setting,
         "assume_relays_are_reachable": False,
         "tunnel_customer_traffic": False,
     }
@@ -76,23 +112,17 @@ def scenario_kwargs():
     if other_settings()["scenario"] == AUTOIMMUNE:
         settings["indirect"] = False
 
-    # Validate Settings
-    if not (settings["relay_asns"] == Peer.twenty or settings["relay_asns"] == Peer.hundred
-            or settings["relay_asns"] == Peer.five or settings["relay_asns"] == Peer.ten):
-        assert not settings["assume_relays_are_reachable"], "assume_relays_are_reachable " \
-                                                            "should only be set True for " \
-                                                            "Peer relay setting"
     return settings
 
 
 def simulation_kwargs():
     return {
         "percent_adoptions": [0.01, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 0.99],
-        "num_trials": 50,
+        "num_trials": 500,
         "subgraphs": [Cls() for Cls in V4Subgraph.v4_subclasses if Cls.name],
-        "parse_cpus": 100,
+        "parse_cpus": 250,
         "python_hash_seed": 0,
-        "caida_kwargs": {}  # {"csv_path": Path("./aux_files/rov_adoption_5.csv")}
+        "caida_kwargs": {"csv_path": Path("./aux_files/rov_adoption_5.csv")} if rov_setting else {}
     }
 
 
@@ -106,6 +136,9 @@ def main():
 
     # Load Simulation settings
     settings = other_settings()
+    settings["output_filename"] = settings["output_filename"] + \
+                                  f"_{scenario_kwargs()['num_attackers']}_attacker" + \
+                                  f"_{simulation_kwargs()['num_trials']}_trials"
 
     sims = None
     if settings["scenario"] == SUBPREFIX_HIJACK:
@@ -132,7 +165,7 @@ def main():
         raise f"Unknown scenario specified: {settings['scenario']}"
 
     # collect experiment settings
-    experiment_settings_to_save = process_experiment_settings(simulation_kwargs(), scenario_kwargs(), other_settings())
+    experiment_settings_to_save = process_experiment_settings(simulation_kwargs(), scenario_kwargs(), settings)
 
     # Run Simulations
     for sim in sims:
