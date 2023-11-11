56e019d5d8a8dbce58e01c4df76adc2fed21a675
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2023-11-11 11:49:49 -0500

    Removed old tests, started migrating first one
    This also involved making some changes to the test_engine. I still need to make a new engine tester. At this point system tests are not operational yet.

--- secure_monitoring_service_pkg/tests/engine_tests/engine_test_configs/config_100.py
+++ secure_monitoring_service_pkg/tests/engine_tests/engine_test_configs/config_100.py
@@ -1,36 +1,32 @@
-from typing import Dict, Type
+from frozendict import frozendict
 
-from bgpy.caida_collector import AS
+from bgpy.tests.engine_tests.graphs import graph_011
+from bgpy.tests.engine_tests.utils import EngineTestConfig
+from bgpy.simulation_engine import BGPSimpleAS, ROVSimpleAS
+from bgpy.enums import ASNs
+from bgpy.simulation_framework import ScenarioConfig, SubprefixHijack
 
-from bgpy import graphs
-from bgpy import EngineTestConfig
-from bgpy import BGPSimpleAS
-from bgpy import ASNs
-
-from rovpp import ROVPPAnn
 from rovpp import ROVPPV1LiteSimpleAS
 
 from secure_monitoring_service_pkg import V4SubprefixHijackScenario
 
 
-class Config100(EngineTestConfig):
-    """Contains config options to run a test"""
-
-    name = "100"
-    desc = "Subprefix Hijack with V1 Lite."
-    scenario = V4SubprefixHijackScenario(
-        attacker_asns={ASNs.ATTACKER.value},
-        victim_asns={ASNs.VICTIM.value},
-        AdoptASCls=ROVPPV1LiteSimpleAS,
+config_100 = EngineTestConfig(
+    name="config_100",
+    desc="Subprefix Hijack with V1 Lite",
+    scenario_config=ScenarioConfig(
+        ScenarioCls=V4SubprefixHijackScenario,
         BaseASCls=BGPSimpleAS,
-        AnnCls=ROVPPAnn,
-    )
-    graph = graphs.Graph011()
-    non_default_as_cls_dict: Dict[int, Type[AS]] = {
-        5: ROVPPV1LiteSimpleAS,
-        6: ROVPPV1LiteSimpleAS,
-        1: ROVPPV1LiteSimpleAS,
-        11: ROVPPV1LiteSimpleAS,
-        12: ROVPPV1LiteSimpleAS,
-    }
-    propagation_rounds = 1
+        AdoptASCls=ROVPPV1LiteSimpleAS,
+        override_attacker_asns=frozenset({ASNs.ATTACKER.value}),
+        override_victim_asns=frozenset({ASNs.VICTIM.value}),
+        override_non_default_asn_cls_dict=frozendict({
+            5: ROVPPV1LiteSimpleAS,
+            6: ROVPPV1LiteSimpleAS,
+            1: ROVPPV1LiteSimpleAS,
+            11: ROVPPV1LiteSimpleAS,
+            12: ROVPPV1LiteSimpleAS,
+        }),
+    ),
+    graph=graph_011,
+)
