519d32e661ff0e0b71032502385b3220773527a4
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2023-11-17 11:56:15 -0500

    Forgot to push changes to __init__.py in engine_test_configs

--- secure_monitoring_service_pkg/tests/engine_tests/engine_test_configs/__init__.py
+++ secure_monitoring_service_pkg/tests/engine_tests/engine_test_configs/__init__.py
@@ -1,12 +1,15 @@
 # Import config variables
 from .config_100 import config_100
+from .config_104 import config_104
 
 # Specify module imports
 __all__ = [
     "config_100",
+    "config_104"
 ]
 
 # Config variable holder
 engine_test_configs = [
     config_100,
+    config_104,
 ]
