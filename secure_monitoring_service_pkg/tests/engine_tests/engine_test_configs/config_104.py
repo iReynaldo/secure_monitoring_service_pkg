95eafd44361a51d035274be4943f4fee178718c4
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2023-11-17 11:11:26 -0500

    Just refactored config_104 and removed unused imports from config_100.

--- secure_monitoring_service_pkg/tests/engine_tests/engine_test_configs/config_104.py
+++ secure_monitoring_service_pkg/tests/engine_tests/engine_test_configs/config_104.py
@@ -1,32 +1,29 @@
 from frozendict import frozendict
 
-from bgpy.tests.engine_tests.graphs import graph_011
+from bgpy.tests.engine_tests.graphs import graph_027
 from bgpy.tests.engine_tests.utils import EngineTestConfig
 from bgpy.simulation_engine import BGPSimpleAS
 from bgpy.enums import ASNs
 from bgpy.simulation_framework import ScenarioConfig
 
-from rovpp import ROVPPV1LiteSimpleAS
-
 from secure_monitoring_service_pkg import V4SubprefixHijackScenario
+from secure_monitoring_service_pkg import ROVSMS
 
-
-config_100 = EngineTestConfig(
-    name="config_100",
-    desc="Subprefix Hijack with V1 Lite",
+config_104 = EngineTestConfig(
+    name="104",
+    desc="Subprefix Hijack with V4 Lite.",
     scenario_config=ScenarioConfig(
         ScenarioCls=V4SubprefixHijackScenario,
         BaseASCls=BGPSimpleAS,
-        AdoptASCls=ROVPPV1LiteSimpleAS,
+        AdoptASCls=ROVSMS,
         override_attacker_asns=frozenset({ASNs.ATTACKER.value}),
         override_victim_asns=frozenset({ASNs.VICTIM.value}),
         override_non_default_asn_cls_dict=frozendict({
-            5: ROVPPV1LiteSimpleAS,
-            6: ROVPPV1LiteSimpleAS,
-            1: ROVPPV1LiteSimpleAS,
-            11: ROVPPV1LiteSimpleAS,
-            12: ROVPPV1LiteSimpleAS,
+            4: ROVSMS,
+            10: ROVSMS,
+            ASNs.VICTIM.value: ROVSMS,
         }),
     ),
-    graph=graph_011,
+    graph=graph_027,
+    propagation_rounds=1,
 )
