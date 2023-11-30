5ad7e0d066256ccc26ed28b3a0425f5cbdc52a1b
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2023-11-30 09:28:26 -0500

    Removed bgp as a standard policy

--- secure_monitoring_service_pkg/__main__.py
+++ secure_monitoring_service_pkg/__main__.py
@@ -50,7 +50,6 @@ SUPERPREFIX_PLUS_PREFIX_HIJACK = "V4SuperprefixPrefixHijack"
 ALL_PERCENTAGES = [0.01, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 0.99]
 
 STANDARD_POLICIES = {
-    'bgp': BGPSimpleAS,
     'rov': ROVSimpleAS,
     'v1lite': ROVPPV1LiteSimpleAS
 }
