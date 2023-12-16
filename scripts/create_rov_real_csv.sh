48238877dc44f34d88f33b259db671da9097e154
Reynaldo Morillo <reynaldo.morillo@uconn.edu>
2023-12-16 03:15:38 -0500

    Updated script on how ROV Real file is created
    Updated script on how ROV Real file is created.
    I forgot to upload this change with the latest rov real csv file.

--- scripts/create_rov_real_csv.sh
+++ scripts/create_rov_real_csv.sh
@@ -3,5 +3,7 @@
 python convert_rov_json_to_csv.py data/rov_info.json
 grep -E 'Friends,[2-7]|ROVISTA|APNIC' data/rov_info.csv > data/rov_info_friends_apnic_rovista.csv
 sed -i 1s/^/"asn,filtering,confidence,source,category\n"/g data/rov_info_friends_apnic_rovista.csv
+sed -i s/1\.0,Friends,4/9\.9,Friends,4/g data/rov_info_friends_apnic_rovista.csv
+sed -i s/1\.0,Friends,5/9\.9,Friends,5/g data/rov_info_friends_apnic_rovista.csv
 cp data/rov_info_friends_apnic_rovista.csv ../secure_monitoring_service_pkg/aux_files/rov_adoption_real.csv
 
